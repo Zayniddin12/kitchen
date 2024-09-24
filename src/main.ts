e9cc72177e66b45c29278f46cdfe47f6d89cd89d
begzod5071 <rafikovbegzod5071@gmail.com>
2024-09-24 17:17:01 +0500

    starter kit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
+import "@/assets/styles/style.scss";
+
+const app = createApp(App);
+
+app
+  .use(ElementPlus)
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
