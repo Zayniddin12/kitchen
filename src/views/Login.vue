486df62411c50d352c6088af0af6e34e3f8a93a9
Dilshod <dilshodbunyodov2020@gmail.com>
2024-09-25 12:35:40 +0500

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import Language from '@/components/language/index.vue'
 
 interface UserData {
   login: string;
@@ -17,55 +18,72 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <div class="p-[32px] !h-[100vh] lg:flex md:flex block items-center relative">
-    <img src="@/assets/images/loginBg.png"
-         class="lg:block md:block hidden h-[100%] lg:w-[50%] md:w-[40%] rounded-lg object-cover" alt="login">
+  <div class="p-8 h-screen flex flex-col lg:flex-row items-center relative bg-[#ffffff]">
+    <Language class="fixed top-[32px] right-[32px]"/>
 
-    <div class="lg:w-[25%] md:w-[50%] w-full m-auto">
-      <div class="flex items-center mb-[24px]">
-        <img src="@/assets/images/logo.png" alt="logo" class="shrink-1">
-        <div class="flex flex-col ml-[12px]">
-          <b class="text-[#000D24] text-[18px] mb-0">НГМК</b>
+    <!-- Background Image Section -->
+    <img src="@/assets/images/loginBg.png"
+         class="hidden lg:block h-full lg:w-1/2 object-cover rounded-lg"
+         alt="login">
+
+    <!-- Login Form Section -->
+    <div class="w-full lg:w-1/4 md:w-1/2 m-auto">
+      <!-- Logo and Title -->
+      <header class="flex items-center mb-6">
+        <img src="@/assets/images/logo.png" alt="logo">
+        <div class="flex flex-col ml-3">
+          <b class="text-[#000D24] text-lg">НГМК</b>
           <span class="text-[#CBCCCE]">Фонд</span>
         </div>
-      </div>
+      </header>
 
-      <b class="text-[#000D24] text-[24px] font-500 text-left">Войти в аккаунт</b><br>
-      <span class="text-[#A8AAAE] text-[14px] font-400">Введите свои учетные данные для доступа к вашей учетной записи</span>
+      <!-- Form Title -->
+      <h1 class="text-[#000D24] text-xl font-bold">Войти в аккаунт</h1>
+      <p class="text-[#A8AAAE] text-[14px] mt-[6px] w-[90%]">Введите свои учетные данные для доступа к вашей учетной записи</p>
 
-      <form @submit.prevent="onSubmit" class="mt-[24px]">
-        <div class="flex-col flex">
-          <label class="text-[#A8AAAE] text-[14px]">Логин</label>
-          <input placeholder="Введите" class="custom-input mt-[4px]" v-model="userData.login" required/>
+      <!-- Login Form -->
+      <form @submit.prevent="onSubmit" class="mt-6">
+        <!-- Username Input -->
+        <div class="flex flex-col mb-4">
+          <label class="text-[#A8AAAE] text-sm">Логин</label>
+          <input placeholder="Введите" class="custom-input mt-1" v-model="userData.login" required/>
         </div>
 
-        <div class="flex-col flex mt-[16px]">
-          <label class="text-[#A8AAAE] text-[14px]">Пароль</label>
-          <input placeholder="Введите" class="custom-input mt-[4px]" v-model="userData.password" required/>
-        </div>
-        <div class="text-right">
-          <div class="text-[#2E90FA] text-[12px] mt-[4px] cursor-pointer block">Забыли пароль?</div>
+        <!-- Password Input -->
+        <div class="flex flex-col mb-2">
+          <label class="text-[#A8AAAE] text-sm">Пароль</label>
+          <input type="password" placeholder="Введите" class="custom-input mt-1" v-model="userData.password" required/>
         </div>
 
-        <div class="mt-[24px] text-center">
-          <button type="submit" class="w-full bg-[#2E90FA] py-[10px] text-white rounded-lg">Войти</button>
+        <!-- Forgot Password Link -->
+        <div class="text-right text-[#2E90FA] text-xs mt-1 cursor-pointer">Забыли пароль?</div>
 
-          <span class="text-[#7F7D83] py-[12px] flex items-center justify-between">
-            <img src="@/assets/images/line.svg" alt="line">
-            или
-            <img src="@/assets/images/line.svg" alt="line">
-          </span>
-
-          <button class="w-full bg-[#4825C2] text-white py-[10px] flex items-center justify-center rounded-lg">
-            ONE
-            <img class="ml-[8px]" src="@/assets/images/oneId.svg" alt="oneId">
-          </button>
+        <!-- Login Button -->
+        <div class="mt-6">
+          <button type="submit" class="w-full bg-[#2E90FA] py-2.5 text-white rounded-lg">Войти</button>
         </div>
+
+        <!-- Divider with text -->
+        <div class="flex items-center justify-between text-[#7F7D83] text-sm mt-4">
+          <img src="@/assets/images/line.svg" alt="line">
+          <span class="px-2">или</span>
+          <img src="@/assets/images/line.svg" alt="line">
+        </div>
+
+        <!-- OneID Button -->
+        <button class="w-full bg-[#4825C2] py-2.5 flex items-center justify-center text-white rounded-lg mt-4">
+          ONE
+          <img class="ml-2" src="@/assets/images/oneId.svg" alt="oneId">
+        </button>
       </form>
     </div>
 
-    <div class="text-[#A8AAAE] text-[14px] absolute right-[32px] bottom-[42px]">©{{ new Date().getFullYear()}} Anysoft</div>
+    <!-- Footer Section -->
+    <footer class="text-[#A8AAAE] text-sm absolute right-8 bottom-10">
+      ©{{ new Date().getFullYear() }} Anysoft
+    </footer>
   </div>
+
 </template>
 
 <style scoped>
