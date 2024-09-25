ad77e5a6d4c821256022d90e6a01f6b285ecec4e
Abbos6465 <abbosmehmonov1@gmail.com>
2024-09-25 10:44:30 +0500

    started input component

---
+++ src/components/ui/form/form.types.ts
@@ -0,0 +1,14 @@
+export interface FormItemType {
+    label?: string;
+    placeholder?: string;
+    disabled?: boolean;
+    required?: boolean;
+    readonly?: boolean;
+}
+
+export interface ElFormItemType extends FormItemType {
+    size?: "large" | "small",
+    clearable?: boolean,
+    name?: string,
+    suffixIcon?: string,
+}
