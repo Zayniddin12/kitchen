1ce6d4f5ec4703613c4762352ee275ec717e2bdd
begzod5071 <rafikovbegzod5071@gmail.com>
2024-09-25 15:13:38 +0500

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainLayout from "@/layout/MainLayout.vue";
+import LoginLayout from "@/layout/LoginLayout.vue";
+import { onMounted, ref, watch } from "vue";
+
+const layouts = {
+  MainLayout: MainLayout,
+  LoginLayout: LoginLayout,
+};
+</script>
 <template>
-  <RouterView />
+  <component :is="layouts[<string>$route.meta.layout]">
+    <RouterView />
+  </component>
+
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,30 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      redirect: () => {
+
+        return { name: "home" };
+      },
+    },
+    {
+      path: "/home",
+      name: "home",
+      component: () => import("@/modules/Home/index.vue"),
+      meta: {
+        layout: "MainLayout",
+        breadcrumb: [
+          {
+            label: "home",
+            isActionable: false,
+          },
+        ],
+
+      },
+    },
+  ],
 });
 
 export default router;
