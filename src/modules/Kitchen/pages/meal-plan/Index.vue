70dd986554f18e2ae1e13fa3adca340b31b859dd
abbosmehmonov <abbosmehmonov1@gmail.com>
2024-10-03 17:23:52 +0500

    added kitchen code

--- src/modules/Kitchen/pages/meal-plan/Index.vue
+++ src/modules/Kitchen/pages/meal-plan/Index.vue
@@ -3,11 +3,15 @@
     lang="ts"
 >
 
-import {computed, ref, watch} from "vue";
-import {onBeforeRouteUpdate, useRoute} from "vue-router";
-import {useKitchenStore} from "@/modules/Kitchen/store/kitchen.store";
+import { computed, ref, watch } from "vue";
+import { onBeforeRouteUpdate, useRoute } from "vue-router";
+import { useKitchenStore } from "@/modules/Kitchen/store/kitchen.store";
 import mailPlanImg from "@/assets/images/mail-plan.png";
 import PlusIcon from "@/assets/images/icons/plus.svg";
+import SellIcon from "@/assets/images/icons/sell-icon.svg";
+import ClockIcon from "@/assets/images/icons/clock.svg";
+import EditIcon from "@/assets/images/icons/edit.svg";
+import { TableColumnType } from "@/types/common.type";
 
 const kitchenStore = useKitchenStore();
 
@@ -45,12 +49,12 @@ const getActiveTab = () => {
 
 watch(() => route.query.tab, () => {
   getActiveTab();
-}, {immediate: true});
+}, { immediate: true });
 
 onBeforeRouteUpdate((to, from, next) => {
   kitchenStore.fetchPart(+to.params.department_id, +to.params.part_id);
 
-  if (!kitchenStore.part) return next({name: "notFound"});
+  if (!kitchenStore.part) return next({ name: "notFound" });
 
   to.meta.breadcrumb = [
     {
@@ -77,7 +81,48 @@ onBeforeRouteUpdate((to, from, next) => {
   next();
 });
 
-const hasData = ref(false);
+const hasData = ref(true);
+
+const currentTabTableColumns = computed<TableColumnType[]>(() => {
+  return [
+    {
+      label: "Название",
+      prop: "name"
+    },
+    {
+      label: "Количество",
+      prop: "quantity"
+    },
+    {
+      label: "Ед. измерения",
+      prop: "unit_measurement"
+    },
+    {
+      label: "Цена",
+      prop: "price"
+    },
+    {
+      label: "Сумма",
+      prop: "sum"
+    }
+  ];
+});
+
+const currentTabTableData = computed(() => {
+  const dataList = [];
+
+  for (let i = 0; i < 4; i++) {
+    dataList.push({
+      name: "Кабачки",
+      quantity: 0.800,
+      unit_measurement: "грамм",
+      price: "1 800 сум",
+      sum: "15 000 сум"
+    });
+  }
+
+  return dataList;
+});
 
 </script>
 
@@ -87,19 +132,214 @@ const hasData = ref(false);
       <h1 class="font-semibold text-[32px] text-dark">
         Меню
       </h1>
-      <div class="app-tabs mt-6 !inline-flex">
-        <RouterLink
-            v-for="item in tabItems"
-            :key="item.value"
-            :class="['app-tab', {'app-tab--active': activeTab === item.value}]"
-            :to="{query: {...route.query, ...{tab: item.value}}}"
+      <div class="mt-6 flex items-center justify-between gap-x-5">
+        <div class="app-tabs !inline-flex">
+          <RouterLink
+              v-for="item in tabItems"
+              :key="item.value"
+              :class="['app-tab', {'app-tab--active': activeTab === item.value}]"
+              :to="{query: {...route.query, ...{tab: item.value}}}"
+          >
+            {{ item.name }}
+          </RouterLink>
+        </div>
+        <div
+            v-if="hasData"
+            class="flex items-center"
         >
-          {{ item.name }}
-        </RouterLink>
+          <template v-if="activeTab === TABS.CURRENT">
+            <ElButton
+                class="!bg-blue-500 min-h-12 w-[253px]"
+                type="primary"
+                size="large"
+            >
+              <div class="flex items-center gap-x-2">
+                <svg
+                    :data-src="PlusIcon"
+                    class="size-6"
+                />
+                <span class="text-lg font-medium">
+                Приготовить блюды
+              </span>
+              </div>
+            </ElButton>
+            <ElButton
+                class="!bg-[#28C76F] min-h-12 w-[149px]"
+                size="large"
+                type="success"
+            >
+              <div class="flex items-center gap-x-2">
+                <svg
+                    :data-src="SellIcon"
+                    class="size-6"
+                />
+                <span class="text-lg font-medium">
+                Продать
+              </span>
+              </div>
+            </ElButton>
+          </template>
+          <template v-else-if="activeTab === TABS.ALL">
+            <ElButton
+                class="min-h-12 w-[253px] !bg-[#E2E6F3] border-none"
+                size="large"
+            >
+              <div class="flex items-center gap-x-2">
+                <svg
+                    :data-src="EditIcon"
+                    class="size-6"
+                />
+                <span class="text-dark-gray font-medium text-lg">
+                  Редактировать
+                </span>
+              </div>
+            </ElButton>
+          </template>
+        </div>
       </div>
       <div class="mt-6">
+        <TransitionGroup
+            v-if="hasData"
+            name="nested"
+            :duration="{ enter: 500, leave: 1500 }"
+            tag="div"
+            class="relative overflow-x-hidden mt-6"
+        >
+          <div
+              v-if="activeTab === TABS.CURRENT"
+              class="inner"
+          >
+            <div class="flex flex-col gap-y-8">
+              <div
+                  v-for="n in 3"
+                  :key="n"
+                  class="border rounded-2xl p-4 pb-6 border-[#E2E6F3]"
+              >
+                <div class="flex justify-between gap-x-5">
+                  <div class="flex flex-col gap-y-2">
+                    <h3 class="font-semibold text-lg text-dark">
+                      Рацион {{ n }} R-{{ Math.floor(Math.random() * 1001) + 1000 }}
+                    </h3>
+                    <div class="flex items-center gap-x-3 font-medium text-sm text-cool-gray">
+                      <div class="flex items-center gap-x-1">
+                        <svg
+                            :data-src="ClockIcon"
+                            class="size-5"
+                        />
+                        <span>
+                          08:00-10:00
+                        </span>
+                      </div>
+                      <span>
+                        Завтрак
+                      </span>
+                    </div>
+                  </div>
+                  <h3 class="font-semibold text-lg text-dark">
+                    25 000 UZS
+                  </h3>
+                </div>
+                <ElTable
+                    :data="currentTabTableData"
+                    class="custom-element-table mt-6"
+                >
+                  <ElTableColumn
+                      v-for="column in currentTabTableColumns"
+                      :key="column.prop"
+                      :label="column.label"
+                      :prop="column.prop"
+                  />
+                </ElTable>
+                <div class="rounded-2xl p-4 bg-white-blue flex justify-between gap-x-24 mt-6">
+                  <div class="flex flex-col gap-y-2 text-sm">
+                    <p>
+                      <span class="text-cool-gray">
+                        Всего порций:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        200
+                      </strong>
+                    </p>
+                    <p>
+                      <span class="text-cool-gray">
+                        Сумма:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        5 000 000 UZS
+                      </strong>
+                    </p>
+                  </div>
+                  <div class="flex flex-col gap-y-2 text-sm">
+                    <p>
+                      <span class="text-cool-gray">
+                        Выданние:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        200
+                      </strong>
+                    </p>
+                    <p>
+                      <span class="text-cool-gray">
+                        Сумма:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        5 000 000 UZS
+                      </strong>
+                    </p>
+                  </div>
+                  <div class="flex flex-col gap-y-2 text-sm">
+                    <p>
+                      <span class="text-cool-gray">
+                        Проданние:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        200
+                      </strong>
+                    </p>
+                    <p>
+                      <span class="text-cool-gray">
+                        Сумма:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        5 000 000 UZS
+                      </strong>
+                    </p>
+                  </div>
+                  <div class="flex flex-col gap-y-2 text-sm">
+                    <p>
+                      <span class="text-cool-gray">
+                        Остатки порций:
+                      </span>
+                      <strong class="font-semibold text-[#EA5455]">
+                        8
+                      </strong>
+                    </p>
+                    <p>
+                      <span class="text-cool-gray">
+                        Сумма:
+                      </span>
+                      <strong class="font-semibold text-dark">
+                        5 000 000 UZS
+                      </strong>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+              v-else-if="activeTab === TABS.ALL"
+              class="inner"
+          >
+           <ElScrollbar>
+             <div class="flex">
+               <div class="py-2 px-4 rounded-lg"></div>
+             </div>
+           </ElScrollbar>
+          </div>
+        </TransitionGroup>
         <div
-            v-if="!hasData"
+            v-else
             class="mx-auto mt-[100px] w-[342px] text-center"
         >
           <img
@@ -128,25 +368,6 @@ const hasData = ref(false);
             </div>
           </ElButton>
         </div>
-        <TransitionGroup
-            name="nested"
-            :duration="{ enter: 500, leave: 1500 }"
-            tag="div"
-            class="relative overflow-x-hidden"
-        >
-          <div
-              v-if="activeTab === TABS.CURRENT"
-              class="inner"
-          >
-
-          </div>
-          <div
-              v-else-if="activeTab === TABS.ALL"
-              class="inner"
-          >
-
-          </div>
-        </TransitionGroup>
       </div>
     </div>
   </section>
