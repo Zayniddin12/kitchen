619abc5fe3c1b97adb2d7cdb81c8c0dedc4baee0
begzod5071 <rafikovbegzod5071@gmail.com>
2024-10-21 18:52:52 +0500

    refactoring

--- src/modules/Settings/store/index.ts
+++ src/modules/Settings/store/index.ts
@@ -1,26 +1,84 @@
-import {defineStore} from "pinia";
+import { defineStore } from "pinia";
 import $axios from "@/plugins/axios";
-import {ref} from "vue";
+import { ref } from "vue";
 
 export const useSettingsStore = defineStore("settingsStore", () => {
 
-    // dilshod
-    const typeProduct = ref<Array[]>([])
-    const vidProduct = ref<Array[]>([
-        {
-            id: 1,
-            photo: '',
-            name: {
-                uz: 'Куриное мясо',
-                ru: 'Куриное мясо',
-            },
-            type: "Мясные",
-        },
-    ])
-    const typeProductDetail = ref({})
-    // dilshod
+  // dilshod
+  const typeProduct = ref<Array[]>([]);
+  const vidProduct = ref<Array[]>([
+    {
+      id: 1,
+      photo: "",
+      name: {
+        uz: "Куриное мясо",
+        ru: "Куриное мясо",
+      },
+      type: "Мясные",
+    },
+  ]);
+  const typeProductDetail = ref({});
+  // dilshod end
 
 
+  // begzod
+  const typeDocument = ref<object[]>([]);
+  const vidDocument = ref<object[]>([]);
+
+  // begzod end
+
+
+  // begzod
+
+  // Документы Типы документов
+  const GET_TYPE_DOCUMENT = async (params: any) => {
+    const { data } = await $axios.get("/documents/categories", { params });
+    typeDocument.value = data;
+  };
+
+
+  const GET_VID_DOCUMENT = async (params: any) => {
+    const { data } = await $axios.get("/documents", { params });
+    vidDocument.value = data;
+  };
+
+
+  // begzod end
+
+  // dilshod
+  // Продукты Типы документов
+  const GET_TYPE_PRODUCT = async (params: any) => {
+    const { data } = await $axios.get("/product-types/categories", { params });
+    typeProduct.value = data;
+  };
+
+  const GET_TYPE_PRODUCT_DETAIL = async (id: number) => {
+    const { data } = await $axios.get(`/get/${id}/`);
+    typeProductDetail.value = data;
+  };
+
+  const CREATE_TYPE_PRODUCT = (data: any) => {
+    return $axios.post("/product-types/categories", data);
+  };
+
+  const UPDATE_TYPE_PRODUCT = ({ id, data }: { id: string | number; data: any }) => {
+    return $axios.patch(`/update/${id}/`, data);
+  };
+
+  const DELETE_TYPE_PRODUCT = (id: number) => {
+    return $axios.delete(`/delete/${id}/`);
+  };
+
+  // Продукты Виды документов
+
+  const GET_VID_PRODUCT = async (params: any) => {
+    const { data } = await $axios.get("/getApi", { params });
+    vidProduct.value = data;
+  };
+  // dilshod end
+
+
+  return {
     // begzod
 
 
@@ -28,56 +86,16 @@ export const useSettingsStore = defineStore("settingsStore", () => {
 
 
     // dilshod
-    // Продукты Типы документов
-    const GET_TYPE_PRODUCT = async (params: any) => {
-        const {data} = await $axios.get('/product-types/categories', {params})
-        typeProduct.value = data
-    }
+    typeProduct,
+    typeProductDetail,
+    GET_TYPE_PRODUCT,
+    GET_TYPE_PRODUCT_DETAIL,
+    CREATE_TYPE_PRODUCT,
+    UPDATE_TYPE_PRODUCT,
+    DELETE_TYPE_PRODUCT,
 
-    const GET_TYPE_PRODUCT_DETAIL = async (id: number) => {
-        const {data} = await $axios.get(`/get/${id}/`)
-        typeProductDetail.value = data
-    }
-
-    const CREATE_TYPE_PRODUCT = (data: any) => {
-        return $axios.post('/product-types/categories', data)
-    }
-
-    const UPDATE_TYPE_PRODUCT = ({id, data}: { id: string | number; data: any }) => {
-        return $axios.patch(`/update/${id}/`, data)
-    }
-
-    const DELETE_TYPE_PRODUCT = (id: number) => {
-        return $axios.delete(`/delete/${id}/`)
-    }
-
-    // Продукты Виды документов
-
-    const GET_VID_PRODUCT = async (params: any) => {
-        const {data} = await $axios.get('/getApi', {params})
-        vidProduct.value = data
-    }
+    vidProduct,
+    GET_VID_PRODUCT,
     // dilshod end
-
-
-    return {
-        // begzod
-
-
-        // begzod end
-
-
-        // dilshod
-        typeProduct,
-        typeProductDetail,
-        GET_TYPE_PRODUCT,
-        GET_TYPE_PRODUCT_DETAIL,
-        CREATE_TYPE_PRODUCT,
-        UPDATE_TYPE_PRODUCT,
-        DELETE_TYPE_PRODUCT,
-
-        vidProduct,
-        GET_VID_PRODUCT
-        // dilshod end
-    }
-})
+  };
+});
