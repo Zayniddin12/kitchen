9bafd53cba1b79f6bc7527b5207d77c002cc981d
Abbos6465 <abbosmehmonov1@gmail.com>
2024-12-23 10:48:26 +0500

    merge commit

--- src/modules/Auth/auth.types.ts
+++ src/modules/Auth/auth.types.ts
@@ -29,10 +29,10 @@ export interface AuthCreateDataType extends AuthLoginDataType {
 export type PositionType = string | null;
 
 export interface UserWorkplaceType {
-    position: PositionType,
-    workplace: string,
-    workplace_id: number,
-    workplace_type: string
+    position: PositionType;
+    workplace: string;
+    workplace_id: number;
+    workplace_type: string;
 }
 
 export interface UserType {
@@ -49,7 +49,7 @@ export interface UserType {
     position_id: number;
     organization_id: number;
     organization_name: string;
-    workplaces: UserWorkplaceType[]
+    workplaces: UserWorkplaceType[];
 }
 
 export interface SendCodeDataType {
@@ -62,19 +62,19 @@ export interface SendCodeOtpType {
     expires: number;
     code_length: number,
     session_id: string;
-    date: Date,
-    code?: string
+    date: Date;
+    code?: string;
 }
 
 export interface VerifyCodeDataType {
     phone: string;
-    code: string,
-    otp_session_id: string,
+    code: string;
+    otp_session_id: string;
 }
 
 export interface ForgotPasswordDataType extends VerifyCodeDataType {
-    new_password: string,
-    password_confirmation: string
+    new_password: string;
+    password_confirmation: string;
 }
 
 export type CodeType = string;
