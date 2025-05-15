7e49d93f89159a1f24fcb9440ab1a5b0164270b8
Muhammadqobil <saidahmedovmuhammadqobil@gmail.com>
2025-05-15 16:28:34 +0500

    add baseWarehouse

--- src/layout/Bars/NavBar.vue
+++ src/layout/Bars/NavBar.vue
@@ -33,7 +33,6 @@ const docTypeName = ref<string>("");
 const docTypeTitle = ref<string>("");
 
 const openModal = (item: DocTypeListType) => {
-
   switch (item.id) {
     case 1:
       editModal.value = true;
